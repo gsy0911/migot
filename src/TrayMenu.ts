75bba63b940c0256e5568763275d40ba995d8f46
yoshiki <yoshiki0911@gmail.com>
2021-01-22 22:04:34 +0900

    feat: add TrayMenu

---
+++ src/TrayMenu.ts
@@ -0,0 +1,51 @@
+import { app, Tray, Menu, nativeImage } from "electron";
+
+export class TrayMenu {
+	// Create a variable to store our tray
+	// Public: Make it accessible outside of the class;
+	// Readonly: Value can't be changed
+	public readonly tray: Tray;
+
+	// Path where should we fetch our icon;
+	private iconPath = "/assets/icon.png";
+
+	constructor() {
+		this.tray = new Tray(this.createNativeImage());
+		this.tray.setContextMenu(this.createContextMenu());
+	}
+
+	createNativeImage(): nativeImage {
+		// Since we never know where the app is installed,
+		// we need to add the app base path to it.
+		// const path = `${__dirname}${this.iconPath}`;
+		const path = `${app.getAppPath()}${this.iconPath}`;
+		const image = nativeImage.createFromPath(path);
+		// Marks the image as a template image.
+		image.setTemplateImage(true);
+		return image;
+	}
+
+	createContextMenu(): Menu {
+		const contextMenu = Menu.buildFromTemplate([
+			{
+				label: "menu 1",
+				type: "radio",
+				click: () => console.log("clicked"),
+			},
+			{ label: "menu 2", type: "radio" },
+			{ type: "separator" },
+			{
+				label: "sub-menu",
+				submenu: [{ label: "sub-menu 1" }, { label: "sub-menu2" }],
+			},
+			{
+				label: "Quit",
+				accelerator: "Command+Q",
+				click: function () {
+					app.quit();
+				},
+			},
+		]);
+		return contextMenu;
+	}
+}
