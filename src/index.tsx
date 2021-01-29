cd1c949a3592e2815777103251d4904bec536552
yoshiki <yoshiki0911@gmail.com>
2021-01-29 11:08:15 +0900

    #6 feat: add hash-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from './FoundationStyle';
 import { store } from './store';
@@ -12,7 +13,11 @@ ReactDOM.render(
 	<Provider store={store}>
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
-			<TaskListContainer />
+			<HashRouter>
+				<div>
+					<Route path="/" exact component={TaskListContainer} />
+				</div>
+			</HashRouter>
 		</ThemeProvider>
 	</Provider>,
 	container
