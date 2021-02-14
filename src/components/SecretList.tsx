be85d7b6adae1472798a35a758b4de9f32e8ef07
yoshiki <yoshiki0911@gmail.com>
2021-02-15 01:02:10 +0900

    #15 feat: add interface

--- src/components/SecretList.tsx
+++ src/components/SecretList.tsx
@@ -19,29 +19,48 @@ const region = 'ap-northeast-1';
 const client = new SecretsManagerClient({ region: region });
 
 
+interface SecretsResponse {
+	status: "invalid" | "valid"
+	data?: SecretsData[]
+}
+
+interface SecretsData {
+	name: string
+	values: Value[]
+}
+
+interface Value {
+	key: string
+	value: string
+}
+
 export const SecretListContainer: React.FC = () => {
 	const classes = useStyles();
 
 	const [secretsName, setSecretsName] = useState<string>('');
 	const onChangeSecretsName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
 		setSecretsName(e.currentTarget.value);
-	}, [''])
+	}, [])
 
-	const [secrets, setSecrets] = useState<string>('');
+	const [secrets, setSecrets] = useState<SecretsResponse>({"status": "invalid", "data": []});
 
 	const onClickSecretsNameButton = useCallback(() => {
 		const command = new GetSecretValueCommand({
 			SecretId: secretsName
 		})
 		client.send(command)
-		.then(
-			(data) => {
-				setSecrets(data.SecretString || '')
-			},
-			(error) => {
-				console.log(error)
-			})
-	}, [''])
+		.then(data => {
+			const secretsString = data.SecretString || '{"status": "invalid"}'
+			setSecrets(JSON.parse(secretsString))
+		})
+		.catch(error => {
+			console.log(error)
+			setSecrets({"status": "invalid"})
+		})
+		.finally(() => {
+			console.log("fetched")
+		})
+	}, [secretsName])
 
 	return (
 		<div className={classes.container}>
@@ -51,12 +70,9 @@ export const SecretListContainer: React.FC = () => {
 				variant="outlined"
 				onChange={onChangeSecretsName}
 				/>
-			{secretsName}
 			<Button aria-label="find secrets" onClick={onClickSecretsNameButton}>
 				button
 			</Button>
-			{secrets}
-			{'hello'}
 		</div>
 	)
 }
